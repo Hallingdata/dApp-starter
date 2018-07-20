128365130e0197411a802d9338009cbf19d565ed
Asgeir Sognefest <sogasg@gmail.com>
2018-07-20 12:05:57 +0200

    Simplify the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
 import * as React from "react"
-import { DrizzleProvider } from "drizzle-react"
+import { SFC } from "react"
 
+import { DrizzleProvider } from "drizzle-react"
 import { LoadingContainer } from "drizzle-react-components"
 import ContractInteraction from "./components/ContractInteraction"
-
 import store from "./store"
 import Layout from "./components/Layout"
 import drizzleOptions from "./drizzleOptions"
 
-class App extends React.Component {
-  render() {
-    return (
-      <DrizzleProvider options={drizzleOptions} store={store}>
-        <LoadingContainer>
-          <Layout>
-            <div>
-              <header>
-                <h1>Demo</h1>
-              </header>
-              <ContractInteraction />
-            </div>
-          </Layout>
-        </LoadingContainer>
-      </DrizzleProvider>
-    )
-  }
-}
+const App: SFC = () => (
+  <DrizzleProvider options={drizzleOptions} store={store}>
+    <LoadingContainer>
+      <Layout>
+        <header>
+          <h1>Demo</h1>
+        </header>
+        <ContractInteraction />
+      </Layout>
+    </LoadingContainer>
+  </DrizzleProvider>
+)
 
 export default App
